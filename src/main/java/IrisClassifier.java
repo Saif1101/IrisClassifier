ce38a9c874c3cb51ff167b65f79510442b703a43
ShaunakInamdar <shaunak.inamdar@gmail.com>
2021-05-01 11:42:10 +0530

    added NN

--- src/main/java/IrisClassifier.java
+++ src/main/java/IrisClassifier.java
@@ -3,14 +3,28 @@ import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class IrisClassifier {
+
+    private static int FEATURES_COUNT = 4;
+    private static int CLASSES_COUNT = 3;
     public static void main(String[] args) {
         BasicConfigurator.configure(); // Logger required by DL4J
         loadData();
@@ -40,9 +54,39 @@ public class IrisClassifier {
             DataSet trainingData = testAndTrain.getTrain();
             DataSet testingData = testAndTrain.getTest();
 
+            //calling model
+            irisNN(trainingData, testingData);
+
         }
         catch (Exception e) {
             System.out.println("Error: " + e.getLocalizedMessage());
         }
+    }
+
+    public static void irisNN (DataSet trainingData, DataSet testingData){
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                .activation(Activation.TANH)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Nesterovs(0.1,0.9))
+                .l2(0.0001)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(FEATURES_COUNT).nOut(3).build())
+                .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
+                .layer(2, new OutputLayer.Builder(
+                        LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).activation(Activation.SOFTMAX)
+                        .nIn(3).nOut(CLASSES_COUNT).build())
+                .backprop(true).pretrain(false)
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(configuration);
+        model.init();
+        model.fit(trainingData);
+
+        //testing the model
+
+        INDArray output = model.output(testingData.getFeatureMatrix());
+        Evaluation eval = new Evaluation(3);
+        eval.eval(testingData.getLabels(), output);
+        System.out.println(eval.stats());
     }
 }
